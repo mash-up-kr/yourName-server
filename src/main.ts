22f764ae8c0e4229a67f7cb21cd669d3e57f115e
god9599 <59276134+god9599@users.noreply.github.com>
2021-09-23 23:51:12 +0900

    Chore: swagger 연동

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
@@ -6,6 +7,14 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('yourName API')
+    .setDescription('The yourNames API description')
+    .setVersion('1.0')
+    .addTag('yourName')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(port);
   console.log(`==============listening on port ${port}====================`);
   if (module.hot) {
