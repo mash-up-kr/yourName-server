57805dad2883d19897d097d89cb7bded78791249
god9599 <59276134+god9599@users.noreply.github.com>
2021-09-25 18:24:02 +0900

    Chore: db 연결

---
+++ ormconfig.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT | 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [path.join(__dirname, '**/*.entity{.ts,.js}')],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  cli: {
+    migrationsDir: __dirname + '/src/migrations',
+  },
+  autoLoadEntities: true,
+  charset: 'utf8mb4',
+  synchronize: false,
+  logging: false,
+};
+
+export = config;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from '../ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(ormconfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
