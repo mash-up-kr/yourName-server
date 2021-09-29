f21691d007128cd73a7a08ddf8b7ac3f06521ac3
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-09-29 15:48:15 +0900

    Chore: 실행 환경에 따라 다른 설정 불러오도록 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as ormconfig from '../ormconfig';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.dev',
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
     TypeOrmModule.forRoot(ormconfig),
   ],
   controllers: [AppController],
