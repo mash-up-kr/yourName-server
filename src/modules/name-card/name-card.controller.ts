7b9c33c7232391215329414ea152a556724a4b0b
marades <jkl9510357@gmail.com>
2021-10-03 02:27:04 +0900

    Feat: 내 명함 목록 api 추가

--- src/modules/name-card/name-card.controller.ts
+++ src/modules/name-card/name-card.controller.ts
@@ -1,13 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CreateNameCardDto } from './dto/create-name-card.dto';
 import { NameCardService } from './name-card.service';
 
-@Controller('name-card')
+@ApiTags('NameCard - 명함')
+@Controller('namecards')
 export class NameCardController {
   constructor(private readonly nameCardService: NameCardService) {}
 
+  @ApiOperation({ summary: '내 명함 생성' })
   @Post()
   async createNameCard(@Body() createNameCardDto: CreateNameCardDto) {
     return await this.nameCardService.createNameCard(createNameCardDto);
+  }
+
+  // auth guard 붙이면 query.userId 교체
+  @ApiOperation({
+    summary: '내 명함 가져오기',
+  })
+  @ApiQuery({
+    name: 'userId',
+    required: true,
+  })
+  @Get()
+  async getMyNameCards(@Query('userId') userId?: number) {
+    return await this.nameCardService.getMyNameCards(userId);
   }
 }
