1485b082b4d779555df47bd5d8f4d181bd45d565
god9599 <59276134+god9599@users.noreply.github.com>
2021-10-03 05:24:24 +0900

    Style: insert tmi data

--- src/database/seeds/create-initial-tmi-data.ts
+++ src/database/seeds/create-initial-tmi-data.ts
@@ -8,247 +8,49 @@ export class CreateInitialTmiData implements Seeder {
       .createQueryBuilder()
       .insert()
       .into(Tmi)
-      .values([{ id: 1, type: '취미 / 관심사', name: '음악' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 2, type: '취미 / 관심사', name: '게임' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 3, type: '취미 / 관심사', name: '영상' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 4, type: '취미 / 관심사', name: '운동' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 5, type: '취미 / 관심사', name: '음식' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 6, type: '취미 / 관심사', name: '요리' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 7, type: '취미 / 관심사', name: '카페' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 8, type: '취미 / 관심사', name: '술' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 9, type: '취미 / 관심사', name: '자연' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 10, type: '취미 / 관심사', name: '야외활동' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 11, type: '취미 / 관심사', name: '봉사' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 12, type: '취미 / 관심사', name: '여행' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 13, type: '취미 / 관심사', name: '동물' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 14, type: '취미 / 관심사', name: '기술' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 15, type: '취미 / 관심사', name: '공연' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 16, type: '취미 / 관심사', name: '덕질' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 17, type: '취미 / 관심사', name: '쇼핑' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 18, type: '취미 / 관심사', name: '예술' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 19, type: '취미 / 관심사', name: '뷰티' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 20, type: '취미 / 관심사', name: '패션' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 21, type: '취미 / 관심사', name: '인테리어' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 22, type: '취미 / 관심사', name: '독서' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 23, type: '취미 / 관심사', name: '만화' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 24, type: '취미 / 관심사', name: '개그' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 25, type: '취미 / 관심사', name: '자동차' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 26, type: '취미 / 관심사', name: '외국어' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 27, type: '취미 / 관심사', name: '경제' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 28, type: '성격', name: '리더쉽' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 29, type: '성격', name: '열정' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 30, type: '성격', name: '끈기' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 31, type: '성격', name: '배려' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 32, type: '성격', name: '추진력' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 33, type: '성격', name: '자신감' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 34, type: '성격', name: '책임감' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 35, type: '성격', name: '친화력' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 36, type: '성격', name: '발표왕' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 37, type: '성격', name: '적응력' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 38, type: '성격', name: '꼼꼼함' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 39, type: '성격', name: '소통' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 40, type: '성격', name: '긍정적' }])
-      .execute();
-    await connection
-      .createQueryBuilder()
-      .insert()
-      .into(Tmi)
-      .values([{ id: 41, type: '성격', name: '아이디어' }])
+      .values([
+        { id: 1, type: '취미 / 관심사', name: '음악' },
+        { id: 2, type: '취미 / 관심사', name: '게임' },
+        { id: 3, type: '취미 / 관심사', name: '영상' },
+        { id: 4, type: '취미 / 관심사', name: '운동' },
+        { id: 5, type: '취미 / 관심사', name: '음식' },
+        { id: 6, type: '취미 / 관심사', name: '요리' },
+        { id: 7, type: '취미 / 관심사', name: '카페' },
+        { id: 8, type: '취미 / 관심사', name: '술' },
+        { id: 9, type: '취미 / 관심사', name: '자연' },
+        { id: 10, type: '취미 / 관심사', name: '야외활동' },
+        { id: 11, type: '취미 / 관심사', name: '봉사' },
+        { id: 12, type: '취미 / 관심사', name: '여행' },
+        { id: 13, type: '취미 / 관심사', name: '동물' },
+        { id: 14, type: '취미 / 관심사', name: '기술' },
+        { id: 15, type: '취미 / 관심사', name: '공연' },
+        { id: 16, type: '취미 / 관심사', name: '덕질' },
+        { id: 17, type: '취미 / 관심사', name: '쇼핑' },
+        { id: 18, type: '취미 / 관심사', name: '예술' },
+        { id: 19, type: '취미 / 관심사', name: '뷰티' },
+        { id: 20, type: '취미 / 관심사', name: '패션' },
+        { id: 21, type: '취미 / 관심사', name: '인테리어' },
+        { id: 22, type: '취미 / 관심사', name: '독서' },
+        { id: 23, type: '취미 / 관심사', name: '만화' },
+        { id: 24, type: '취미 / 관심사', name: '개그' },
+        { id: 25, type: '취미 / 관심사', name: '자동차' },
+        { id: 26, type: '취미 / 관심사', name: '외국어' },
+        { id: 27, type: '취미 / 관심사', name: '경제' },
+        { id: 28, type: '성격', name: '리더쉽' },
+        { id: 29, type: '성격', name: '열정' },
+        { id: 30, type: '성격', name: '끈기' },
+        { id: 31, type: '성격', name: '배려' },
+        { id: 32, type: '성격', name: '추진력' },
+        { id: 33, type: '성격', name: '자신감' },
+        { id: 34, type: '성격', name: '책임감' },
+        { id: 35, type: '성격', name: '친화력' },
+        { id: 36, type: '성격', name: '발표왕' },
+        { id: 37, type: '성격', name: '적응력' },
+        { id: 38, type: '성격', name: '꼼꼼함' },
+        { id: 39, type: '성격', name: '소통' },
+        { id: 40, type: '성격', name: '긍정적' },
+        { id: 41, type: '성격', name: '아이디어' },
+      ])
       .execute();
   }
 }
