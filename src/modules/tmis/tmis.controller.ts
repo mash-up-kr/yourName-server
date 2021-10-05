c85549656991a89aec95e9d8e0916daab6b22499
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-10-05 15:36:33 +0900

    Feat: tmi 조회 스웨거 코드 추가

--- src/modules/tmis/tmis.controller.ts
+++ src/modules/tmis/tmis.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiDocs } from './tmis.docs';
 import { TmisService } from './tmis.service';
 
+@ApiTags('tmi')
 @Controller('tmis')
 export class TmisController {
   constructor(private tmisService: TmisService) {}
 
+  @ApiDocs.getBehaviorTmis('취미/관심사 tmis 조회')
   @Get('behavior')
   async getBehaviorTmis() {
     return this.tmisService.findBehaviorTmis();
   }
 
+  @ApiDocs.getCharacterTmis('성격 tmis 조회')
   @Get('character')
   async getCharacterTmis() {
     return this.tmisService.findCharacterTmis();
