e54f5f00cb75035b2a4ed3bd8df4049deb31a791
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-10-06 17:19:52 +0900

    Chore: collection 테이블을 따로 두는 방식으로 관계 변경

--- src/entities/collection.entity.ts
+++ src/entities/collection.entity.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 import { BaseEntity } from './base.entity';
-import { NameCard } from './name-card.entity';
+import { CollectionNameCard } from './collection-name-card.entity';
 import { User } from './user.entity';
 
 @Entity({ name: 'collection' })
@@ -11,20 +11,15 @@ export class Collection extends BaseEntity {
   @Column()
   userId: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  @Column()
-  nameCardId: number;
-
   @JoinColumn({ name: 'userId', referencedColumnName: 'id' })
   @ManyToOne(() => User, {
     onDelete: 'CASCADE',
   })
   user: User;
 
-  @JoinColumn({ name: 'nameCardId', referencedColumnName: 'id' })
-  @ManyToOne(() => NameCard, {
-    onDelete: 'CASCADE',
-  })
-  nameCard: NameCard;
+  @OneToMany(
+    () => CollectionNameCard,
+    (collectionNameCard) => collectionNameCard.collection,
+  )
+  collectionNameCards: CollectionNameCard[];
 }
