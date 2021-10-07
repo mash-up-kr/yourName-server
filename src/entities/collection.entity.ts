59d24db3943c9fbb77e9a1d12b4de0a9bac74ccb
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-10-07 10:07:12 +0900

    Fix: 사용자-도감 관계 변경

--- src/entities/collection.entity.ts
+++ src/entities/collection.entity.ts
@@ -1,5 +1,5 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
-import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 import { BaseEntity } from './base.entity';
 import { CollectionNameCard } from './collection-name-card.entity';
 import { User } from './user.entity';
@@ -12,7 +12,7 @@ export class Collection extends BaseEntity {
   userId: number;
 
   @JoinColumn({ name: 'userId', referencedColumnName: 'id' })
-  @ManyToOne(() => User, {
+  @OneToOne(() => User, {
     onDelete: 'CASCADE',
   })
   user: User;
