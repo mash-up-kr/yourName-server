2e5ab2245cc1c73159e8ef3a73510e1822c0fd25
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-10-08 16:58:32 +0900

    Chore: image data insert

---
+++ src/database/seeds/create-initial-image-data.ts
@@ -0,0 +1,17 @@
+import { Image } from '../../entities/image.entity';
+import { Connection } from 'typeorm';
+import { Factory, Seeder } from 'typeorm-seeding';
+
+export class CreateInitialImageData implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(Image)
+      .values([
+        { id: 1, url: 'testurl' },
+        { id: 2, url: 'testurl2' },
+      ])
+      .execute();
+  }
+}
