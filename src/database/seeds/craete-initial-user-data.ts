2e112e087c861c025d9e9e98cb41eeef5475e1a2
alwayslee <ddoddo1997@naver.com>
2021-10-11 15:47:42 +0900

    Style: Seeding되게 파일 순서 변경 및 User Seeding Data 변경

--- src/database/seeds/craete-initial-user-data.ts
+++ src/database/seeds/craete-initial-user-data.ts
@@ -1,26 +1,26 @@
-import { Collection } from '../../entities/collection.entity';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
+import { User } from '../../entities/user.entity';
 
-export class CreateInitialCollectionData implements Seeder {
+export class CreateInitialUserData implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
     await connection
       .createQueryBuilder()
       .insert()
-      .into(Collection)
-      .values([{ id: 1, userId: 1 }])
+      .into(User)
+      .values([{ id: 1, nickName: '거뇌', providerName: 'Apple' }])
       .execute();
     await connection
       .createQueryBuilder()
       .insert()
-      .into(Collection)
-      .values([{ id: 2, userId: 2 }])
+      .into(User)
+      .values([{ id: 2, nickName: 'u2', providerName: 'Kakao' }])
       .execute();
     await connection
       .createQueryBuilder()
       .insert()
-      .into(Collection)
-      .values([{ id: 3, userId: 3 }])
+      .into(User)
+      .values([{ id: 3, nickName: 'u3', providerName: 'Apple' }])
       .execute();
   }
 }
