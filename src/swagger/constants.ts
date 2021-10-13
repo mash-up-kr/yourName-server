7a52479c310df1bdc78424dca7bcbf4e71d66d99
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-10-13 17:17:12 +0900

    Refactor: swagger 옵션 변경

--- src/swagger/constants.ts
+++ src/swagger/constants.ts
@@ -1,5 +1,5 @@
 export const SWAGGER_API_ROOT = 'api-docs';
-export const SWAGGER_API_TITLE = 'yourName API';
-export const SWAGGER_API_DESCRIPTION = 'The yourNames API description';
+export const SWAGGER_API_TITLE = 'MEETU API';
+export const SWAGGER_API_DESCRIPTION = 'The MEETU API description';
 export const SWAGGER_API_CURRENT_VERSION = '1.0';
-export const SWAGGER_API_TAG = 'yourName';
+export const SWAGGER_API_TAG = 'MEETU';
