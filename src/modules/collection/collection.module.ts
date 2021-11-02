10d67ad3a0080a5701103a95aba5f8e5a58f2a8a
alwayslee <ddoddo1997@naver.com>
2021-11-02 15:20:54 +0900

    Feat: 도감 생성 및 수정 API에 배경색 반영

--- src/modules/collection/collection.module.ts
+++ src/modules/collection/collection.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BgColor } from 'src/entities/bg-color.entity';
 import { CollectionNameCard } from 'src/entities/collection-name-card.entity';
 import { Collection } from 'src/entities/collection.entity';
 import { NameCard } from 'src/entities/name-card.entity';
@@ -9,7 +10,13 @@ import { CollectionService } from './collection.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Collection, NameCard, CollectionNameCard, User]),
+    TypeOrmModule.forFeature([
+      Collection,
+      NameCard,
+      CollectionNameCard,
+      User,
+      BgColor,
+    ]),
   ],
   controllers: [CollectionController],
   providers: [CollectionService],
