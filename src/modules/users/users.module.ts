0af17b6d6e3cef30fcdbc34a44a4ccf98bd97d80
marades <jkl9510357@gmail.com>
2021-11-05 23:18:44 +0900

    Chore: 안쓰는 모듈 제거

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
 import { UserOnboarding } from 'src/entities/user-onboarding.entity';
 import { BgColor } from 'src/entities/bg-color.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserOnboarding, BgColor])],
+  imports: [TypeOrmModule.forFeature([UserOnboarding, BgColor])],
   controllers: [UsersController],
   providers: [UsersService],
 })
