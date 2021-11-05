506012f7aa2d1ce49e1228082be1d9a39fb4e331
marades <jkl9510357@gmail.com>
2021-11-05 22:44:06 +0900

    Refactor: color4, 5 추가 반영 및 constant 및 type 분리

---
+++ src/constants/userOnboardingImageUrlMap.ts
@@ -0,0 +1,32 @@
+export default {
+  makeFirstNameCard: {
+    WAIT: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient1_lock.png',
+    DONE_WAIT:
+      'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient1_lock.png',
+    DONE: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient1.png',
+  },
+  shareNameCard: {
+    WAIT: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient2_lock.png',
+    DONE_WAIT:
+      'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient2_lock.png',
+    DONE: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient2.png',
+  },
+  addNameCollectionNameCard: {
+    WAIT: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient3_lock.png',
+    DONE_WAIT:
+      'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient3_lock.png',
+    DONE: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient3.png',
+  },
+  makeCollection: {
+    WAIT: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient4_lock.png',
+    DONE_WAIT:
+      'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient4_lock.png',
+    DONE: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient4.png',
+  },
+  makeNamCards: {
+    WAIT: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient5_lock.png',
+    DONE_WAIT:
+      'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient5_lock.png',
+    DONE: 'https://erme.s3.ap-northeast-2.amazonaws.com/user_onboarding/Gradient5.png',
+  },
+};
