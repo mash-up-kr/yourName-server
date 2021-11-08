148a5d274fe12f20d3fa4db23fceed37e67d2bb6
alwayslee <ddoddo1997@naver.com>
2021-11-08 20:28:25 +0900

    Feat: 도감 생성 및 수정 DTO 배경색 ID 추가

--- src/modules/collection/dto/create-collection.dto.ts
+++ src/modules/collection/dto/create-collection.dto.ts
@@ -1,16 +1,8 @@
 import { PickType } from '@nestjs/swagger';
-import { BgColor } from '../../../entities/bg-color.entity';
 import { Collection } from '../../../entities/collection.entity';
 
 export class CreateCollectionDto extends PickType(Collection, [
   'name',
   'description',
-] as const) {
-  bgcolor: BgColorDto;
-}
-
-class BgColorDto extends PickType(BgColor, [
-  'color1',
-  'color2',
-  'color3',
+  'bgColorId',
 ] as const) {}
