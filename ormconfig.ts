66e2c127a191fb1d3f162105efd79446b1b29a3f
SungMin Joo <59276134+god9599@users.noreply.github.com>
2021-11-09 08:39:47 +0900

    Fix: 경로 문제로 인한 임시 엔티티 처리

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as path from 'path';
 import * as dotenv from 'dotenv';
+import { CollectionNameCard } from 'src/entities/collection-name-card.entity';
+import { Collection } from 'src/entities/collection.entity';
+import { Contact } from 'src/entities/contact.entity';
+import { Image } from 'src/entities/image.entity';
+import { NameCardContact } from 'src/entities/name-card-contact.entity';
+import { NameCardTmi } from 'src/entities/name-card-tmi.entity';
+import { NameCard } from 'src/entities/name-card.entity';
+import { PersonalSkill } from 'src/entities/personal-skill.entity';
+import { Skill } from 'src/entities/skill.entity';
+import { Tmi } from 'src/entities/tmi.entity';
+import { User } from 'src/entities/user.entity';
+import { UserOnboarding } from './src/entities/user-onboarding.entity';
+import { BgColor } from './src/entities/bg-color.entity';
 
 dotenv.config();
 const config: TypeOrmModuleOptions = {
@@ -10,7 +22,21 @@ const config: TypeOrmModuleOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [path.join(__dirname, '**/*.entity{.ts,.js}')],
+  entities: [
+    CollectionNameCard,
+    BgColor,
+    Collection,
+    Contact,
+    Image,
+    NameCardContact,
+    NameCardTmi,
+    NameCard,
+    PersonalSkill,
+    Skill,
+    Tmi,
+    User,
+    UserOnboarding,
+  ],
   migrations: [__dirname + '/src/migrations/*.ts'],
   cli: {
     migrationsDir: __dirname + '/src/migrations',
