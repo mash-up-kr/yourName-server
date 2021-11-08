a519554580a1fd04490604a831bb2925d56ef5c9
alwayslee <ddoddo1997@naver.com>
2021-11-08 20:35:24 +0900

    Feat: 응답폼 통일을 위한 Response Global Interceptor 구현

---
+++ src/transform.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  code: number;
+  data: T;
+  message: string;
+}
+
+@Injectable()
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    const statusCode: number = context.getArgs()[1].statusCode;
+    return next
+      .handle()
+      .pipe(
+        map((data) =>
+          data
+            ? { code: statusCode, data: data, message: 'Success' }
+            : { code: statusCode, data: {}, message: 'Success' },
+        ),
+      );
+  }
+}
