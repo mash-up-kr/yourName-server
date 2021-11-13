2bf72a51fab81bc8e955f0803c8ce66229178e6a
alwayslee <ddoddo1997@naver.com>
2021-11-13 21:50:08 +0900

    Chore: auth interface 파일명 변경 및 폴더 이동

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,35 @@
+import { JwtHeader, JwtPayload } from 'jwt-decode';
+
+export interface IdentityTokenSchema extends JwtPayload {
+  nonce: string;
+  c_hash: string;
+  email: string;
+  email_verified: string;
+  is_private_email: string;
+  auth_time: number;
+}
+
+export interface IdentityTokenHeader extends JwtHeader {
+  kid: string;
+}
+
+export interface ProviderDataSchema {
+  nickName: string;
+  providerName: string;
+}
+
+export interface ApplePublicKeyType {
+  keys: Array<{
+    [key: string]: string;
+  }>;
+}
+
+export interface TokenSchema {
+  accessToken: string;
+  refreshToken?: string;
+}
+
+export interface PayloadSchema {
+  userId: number;
+  nickName: string;
+}
