6cf3beec01557ffef445eb2d007095acea9ec7e0
alwayslee <ddoddo1997@naver.com>
2021-11-18 00:01:01 +0900

    Feat: Collection Swagger 코드 추가

--- src/modules/collection/collection.docs.ts
+++ src/modules/collection/collection.docs.ts
@@ -1,7 +1,10 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { SwaggerMethodDoc } from '../../swagger/swagger-method-doc-type';
 import { CollectionController } from './collection.controller';
+import { AddAndRemoveNamecardsDto } from './dto/add-and-remove-namecards.dto';
+import { AddNamecardToCollectionsDto } from './dto/add-namecard-to-collections.dto';
+import { UpsertCollectionDto } from './dto/upsert-collection.dto';
 
 export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
   getCollections(summary: string) {
@@ -9,6 +12,10 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
       ApiOperation({
         summary,
         description: '유저가 보유한 모든 도감들을 조회',
+      }),
+      ApiResponse({
+        description: '유저의 도감들',
+        status: 200,
       }),
     );
   },
@@ -24,6 +31,10 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: '명함의 고유 코드',
       }),
+      ApiResponse({
+        description: '검색한 명함',
+        status: 200,
+      }),
     );
   },
 
@@ -37,6 +48,10 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         name: 'userId',
         required: true,
         description: 'User ID',
+      }),
+      ApiResponse({
+        description: '유저가 추가한 전체 명함들',
+        status: 200,
       }),
     );
   },
@@ -52,6 +67,10 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: 'Collection ID',
       }),
+      ApiResponse({
+        description: '도감에 있는 명함들',
+        status: 200,
+      }),
     );
   },
 
@@ -60,17 +79,14 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
       ApiOperation({
         summary,
         description:
-          '유저가 지정한 이름과 해당 도감에 대한 한줄 설명과 함께 도감을 생성',
+          '유저가 지정한 이름과 해당 도감에 대한 한줄 설명, 배경색과 함께 도감을 생성',
       }),
-      ApiParam({
-        name: 'name',
-        required: true,
-        description: '새로 생성한 도감의 이름',
+      ApiBody({
+        type: UpsertCollectionDto,
       }),
-      ApiParam({
-        name: 'description',
-        required: true,
-        description: '새로 생성한 도감에 대한 설명',
+      ApiResponse({
+        status: 201,
+        description: '생성한 도감의 ID',
       }),
     );
   },
@@ -79,17 +95,14 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
     return applyDecorators(
       ApiOperation({
         summary,
-        description: '해당 ID를 가진 명함에 해당 ID를 가진 명함들을 추가',
+        description: '해당 ID를 가진 도감에 해당 ID를 가진 명함들을 추가',
       }),
-      ApiParam({
-        name: 'collectionId',
-        required: true,
-        description: '도감 ID',
+      ApiBody({
+        type: AddAndRemoveNamecardsDto,
       }),
-      ApiParam({
-        name: 'addNamecardsToCollectionData',
-        required: true,
-        description: 'Namecard ID들이 담긴 DTO',
+      ApiResponse({
+        status: 201,
+        description: '도감-명함 관계 테이블에 추가된 ID들',
       }),
     );
   },
@@ -106,10 +119,12 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: '명함의 고유 코드',
       }),
-      ApiParam({
-        name: 'addNameCardToCollectionsData',
-        required: true,
-        description: 'Collection ID들이 담긴 DTO',
+      ApiBody({
+        type: AddNamecardToCollectionsDto,
+      }),
+      ApiResponse({
+        status: 201,
+        description: '도감-명함 관계 테이블에 추가된 ID들',
       }),
     );
   },
@@ -125,6 +140,13 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: '도감 ID',
       }),
+      ApiBody({
+        type: UpsertCollectionDto,
+      }),
+      ApiResponse({
+        status: 200,
+        description: '빈 data 객체 + Success Message',
+      }),
     );
   },
 
@@ -139,6 +161,10 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: '도감 ID',
       }),
+      ApiResponse({
+        status: 200,
+        description: '빈 data 객체 + Success Message',
+      }),
     );
   },
 
@@ -149,14 +175,16 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         description: '전체 도감에서 선택된 명함들을 제거',
       }),
       ApiParam({
-        name: 'namecardIds',
-        required: true,
-        description: '명함 ID들',
-      }),
-      ApiParam({
         name: 'userId',
         required: true,
         description: '유저 ID',
+      }),
+      ApiBody({
+        type: AddAndRemoveNamecardsDto,
+      }),
+      ApiResponse({
+        status: 200,
+        description: '빈 data 객체 + Success Message',
       }),
     );
   },
@@ -172,10 +200,12 @@ export const ApiDocs: SwaggerMethodDoc<CollectionController> = {
         required: true,
         description: 'Collection ID',
       }),
-      ApiParam({
-        name: 'namecardIds',
-        required: true,
-        description: '명함 ID들',
+      ApiBody({
+        type: AddAndRemoveNamecardsDto,
+      }),
+      ApiResponse({
+        status: 200,
+        description: '빈 data 객체 + Success Message',
       }),
     );
   },
--- src/modules/collection/dto/add-namecard-to-collections.dto.ts
+++ src/modules/collection/dto/add-namecard-to-collections.dto.ts
@@ -1,7 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { number } from 'joi';
 
 export class AddNamecardToCollectionsDto {
   @IsNumber({}, { each: true })
   @IsNotEmpty()
+  @ApiProperty({ type: number, description: '도감 ID들', example: '[1,2,3]' })
   collectionIds: number[];
 }
