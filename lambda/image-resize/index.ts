844715d67d24e5cb9c28c047632f38c8dc3e7013
marades <jkl9510357@gmail.com>
2021-11-24 21:30:53 +0900

    WIP: Lambda 배포 테스트

--- lambda/image-resize/index.ts
+++ lambda/image-resize/index.ts
@@ -1,1 +1,29 @@
-console.log("zz");
+const sharp = require("sharp");
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+
+const Bucket = "erme";
+
+const width = 188;
+const height = 188;
+
+exports.handler = async (event) => {
+  const key = event.Records[0].s3.object.key;
+  const sanitizedKey = key.replace(/\+/g, " ");
+  const parts = sanitizedKey.split("/");
+  const filename = parts[parts.length - 1];
+
+  const image = await s3.getObject({ Bucket, Key: sanitizedKey }).promise();
+
+  const resizedImage = await sharp(image.Body)
+    .resize({ width, height })
+    .toBuffer();
+
+  return await s3
+    .putObject({
+      Bucket,
+      Body: resizedImage,
+      key: `resized/${filename}`,
+    })
+    .promise();
+};
