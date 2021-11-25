de7cdadb18a4c3aee48d64dc217bba5b75f20b72
alwayslee <ddoddo1997@naver.com>
2021-11-25 15:52:35 +0900

    Feat: User API에 가드 적용, User Swagger에 BearerAuth 추가

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Param, Put } from '@nestjs/common';
+import { Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { BgColor } from 'src/entities/bg-color.entity';
 import { userOnboardingType } from 'src/utils/types';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ApiDocs } from './users.docs';
 import { UsersService } from './users.service';
 
 @ApiTags('User 유저 관련')
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -17,33 +19,38 @@ export class UsersController {
    * - 쓸 수 있는 색깔
    */
   @ApiDocs.getUserOnboardings('온보딩 목록 및 달성 여부 가져오기')
-  @Get(':id/onboarding')
-  async getUserOnboardings(@Param('id') userId: number) {
-    return await this.usersService.getUserOnboardings(userId);
+  @Get('/onboarding')
+  async getUserOnboardings(@Req() req: any) {
+    return await this.usersService.getUserOnboardings(req.user.userId);
   }
 
   @ApiDocs.doneUserOnboarding('온보딩 퀘스트 완료하기')
-  @Put(':id/onboarding/:type/done')
+  @Put('/onboarding/:type/done')
   async doneUserOnboarding(
-    @Param('id') userId: number,
+    @Req() req: any,
     @Param('type') onboardingType: userOnboardingType,
   ) {
-    await this.usersService.doneUserOnboarding(userId, onboardingType);
+    await this.usersService.doneUserOnboarding(req.user.userId, onboardingType);
   }
 
   @ApiDocs.doneWaitUserOnboarding('온보딩 퀘스트 완료 대기 상태로 변경')
-  @Put(':id/onboarding/:type/done-wait')
+  @Put('/onboarding/:type/done-wait')
   async doneWaitUserOnboarding(
-    @Param('id') userId: number,
+    @Req() req: any,
     @Param('type') onboardingType: userOnboardingType,
   ) {
-    await this.usersService.doneWaitUserOnboarding(userId, onboardingType);
+    await this.usersService.doneWaitUserOnboarding(
+      req.user.userId,
+      onboardingType,
+    );
   }
 
   @ApiDocs.getBgColors('유저가 사용한 컬러 조회')
-  @Get(':id/bgcolors')
-  async getBgColors(@Param('id') userId: number) {
-    const bgColors: BgColor[] = await this.usersService.getBgColors(userId);
+  @Get('/bgcolors')
+  async getBgColors(@Req() req: any) {
+    const bgColors: BgColor[] = await this.usersService.getBgColors(
+      req.user.userId,
+    );
     return { list: bgColors };
   }
 }
