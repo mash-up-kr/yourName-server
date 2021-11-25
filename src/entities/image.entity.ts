8d84bff1ca188f79e90a1557ad00da9b7bb2d206
alwayslee <ddoddo1997@naver.com>
2021-11-25 13:58:30 +0900

    Feat: Image API Response Example 추가

--- src/entities/image.entity.ts
+++ src/entities/image.entity.ts
@@ -5,9 +5,9 @@ import { BaseEntity } from './base.entity';
 
 @Entity({ name: 'image' })
 export class Image extends BaseEntity {
+  @ApiProperty({ example: 'profile/OCR 솔루션 기능 비교2 (자동 저장됨).xlsx' })
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ example: 'testUrl' })
   @Column()
   key: string;
 }
