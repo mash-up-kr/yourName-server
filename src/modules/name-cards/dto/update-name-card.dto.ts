12caad4fc8dcfd72c2fc207838d8145b7525926b
god9599 <59276134+god9599@users.noreply.github.com>
2021-11-25 21:08:26 +0900

    Feat: imageId 프로퍼티 추가

--- src/modules/name-cards/dto/update-name-card.dto.ts
+++ src/modules/name-cards/dto/update-name-card.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsNumber, IsOptional } from 'class-validator';
 import { CreateNameCardDto } from './create-name-card.dto';
 
-export class UpdateNameCardDto extends PartialType(CreateNameCardDto) {}
+export class UpdateNameCardDto extends PartialType(CreateNameCardDto) {
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty({
+    example: 1,
+  })
+  imageId: number;
+}
