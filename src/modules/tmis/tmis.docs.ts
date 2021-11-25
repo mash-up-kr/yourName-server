f4cfbb91fa9746bf6661c5d28b5e98c4006a7de8
alwayslee <ddoddo1997@naver.com>
2021-11-25 14:49:56 +0900

    Feat: Tmi API Response Example 추가

--- src/modules/tmis/tmis.docs.ts
+++ src/modules/tmis/tmis.docs.ts
@@ -1,6 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SwaggerMethodDoc } from '../../swagger/swagger-method-doc-type';
+import { GetTmisResponseDto } from './dto/get-tmis-response.dto';
 import { TmisController } from './tmis.controller';
 
 export const ApiDocs: SwaggerMethodDoc<TmisController> = {
@@ -10,6 +11,11 @@ export const ApiDocs: SwaggerMethodDoc<TmisController> = {
         summary,
         description: '취미/관심사에 해당하는 tmi 정보들을 조회합니다',
       }),
+      ApiResponse({
+        status: 200,
+        description: '취미/관심사에 해당하는 tmi 정보들',
+        type: GetTmisResponseDto,
+      }),
     );
   },
   getCharacterTmis(summary) {
@@ -17,6 +23,11 @@ export const ApiDocs: SwaggerMethodDoc<TmisController> = {
       ApiOperation({
         summary,
         description: '성격에 해당하는 tmi 정보들을 조회합니다.',
+      }),
+      ApiResponse({
+        status: 200,
+        description: '성격에 해당하는 tmi 정보들',
+        type: GetTmisResponseDto,
       }),
     );
   },
