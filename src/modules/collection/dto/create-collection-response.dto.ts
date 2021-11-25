61018b59f8b5c7ac28dd715e0efc959ec43809b0
alwayslee <ddoddo1997@naver.com>
2021-11-25 14:07:40 +0900

    Feat: 도감 생성 API 응답 수정

--- src/modules/collection/dto/create-collection-response.dto.ts
+++ src/modules/collection/dto/create-collection-response.dto.ts
@@ -1,7 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { CommonResponseDto } from 'src/common/dto/common-response.dto';
 
+class CollectionIdSchema {
+  @ApiProperty({ example: 6, description: '추가된 도감 id' })
+  collectionId: number;
+}
+
 export class CreateCollectionResponseDto extends CommonResponseDto {
   @ApiProperty({ example: 6, description: '추가된 도감 id' })
-  data: number;
+  data: CollectionIdSchema;
 }
