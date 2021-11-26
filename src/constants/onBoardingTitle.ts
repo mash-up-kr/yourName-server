e2e4b0f776062ff3eff871a7c155c09bce0cc566
alwayslee <ddoddo1997@naver.com>
2021-11-27 01:12:50 +0900

    Refactor: 온보딩 constants들 묶고, 바뀐 온보딩 퀘스트 컬럼명으로 변경

--- src/constants/onBoardingTitle.ts
+++
@@ -1,7 +0,0 @@
-export const OnboardingTitle = {
-  makeFirstNameCard: '나의 첫 미츄 만들기',
-  shareMyNameCard: '나의 미츄 공유하기',
-  addNameCollectionNameCard: '친구 1명 추가하기',
-  makeNewCollection: '새로운 도감 만들기',
-  makeThreeNamCards: '미츄 3개 이상 만들기',
-};
