dc1d42395fd3e504d546461be3f2217c0f4f86d6
god9599 <59276134+god9599@users.noreply.github.com>
2021-11-26 22:27:31 +0900

    Refacor: 온보딩 타이틀 상수 처리 위치 변경

--- src/modules/auth/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export const OnboardingTitle = {
-  makeFirstNameCard: '나의 첫 미츄 만들기',
-  shareNameCard: '나의 미츄 공유하기',
-  addNameCollectionNameCard: '친구 1명 추가하기',
-  makeCollection: '새로운 도감 만들기',
-  makeNamCards: '미츄 3개 이상 만들기',
-};
