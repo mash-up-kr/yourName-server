5a9001c10df95759ca0bf21906e3ecee20c1fac9
alwayslee <ddoddo1997@naver.com>
2021-11-26 13:08:59 +0900

    Refactor: Rename Onboarding Title

--- src/modules/auth/utils/types.ts
+++ src/modules/auth/utils/types.ts
@@ -1,7 +1,7 @@
 export const OnboardingTitle = {
-  makeFirstNameCard: '첫 미츄 만들기',
-  shareNameCard: '미츄 공유하기',
-  addNameCollectionNameCard: '도감에 미츄 추가하기',
-  makeCollection: '도감 만들기',
-  makeNamCards: '미츄 만들기',
+  makeFirstNameCard: '나의 첫 미츄 만들기',
+  shareNameCard: '나의 미츄 공유하기',
+  addNameCollectionNameCard: '친구 1명 추가하기',
+  makeCollection: '새로운 도감 만들기',
+  makeNamCards: '미츄 3개 이상 만들기',
 };
