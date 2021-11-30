14a1d9b84045358f21a5e36877019155340c261e
alwayslee <ddoddo1997@naver.com>
2021-11-30 22:15:11 +0900

    Refactor: response form 필드명 변경에 따른 Swagger 코드 변경

--- src/common/dto/common-response.dto.ts
+++ src/common/dto/common-response.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CommonResponseDto {
   @ApiProperty({ example: `${200} or ${201}` })
-  code: number;
+  statusCode: number;
 
   @ApiProperty({ example: 'Success' })
   message: string;
