2382ac6b5eaef7261ccd2118f8d2f13377074f4c
김건회 <jkl9510357@gmail.com>
2021-12-05 17:18:03 +0900

    Chore: jwt 디코드값 user 엔티티와 통일되도록 / nickanme -> userIdentifier 적용

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { userId: payload.userId, nickName: payload.nickName };
+    return { id: payload.userId, userIdentifier: payload.userIdentifier };
   }
 }
